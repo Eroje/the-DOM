1e6582a86672500bff9968adedd2171bab8ff2cb
Mojid Eroje <majidy287@gmail.com>
2023-08-10 09:00:41 +0100

    added more files

---
+++ Registering-Event-Handler/main.js
@@ -0,0 +1,10 @@
+
+let readMore = document.getElementById('read-more');
+let moreInfo = document.getElementById('more-info');
+
+
+function showInfo() {
+  moreInfo.style.display = 'block';
+}
+
+readMore.addEventListener('click', showInfo);
