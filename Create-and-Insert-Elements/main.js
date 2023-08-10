d770a9dfd219db162db7f2dc2b7957ed65ced646
Mojid Eroje <majidy287@gmail.com>
2023-08-10 05:48:55 +0100

    First Commit

---
+++ Create-and-Insert-Elements/main.js
@@ -0,0 +1,7 @@
+let newAttraction = document.createElement('li'); // Create a list element
+
+newAttraction.id = 'vespa'; // assigns an id to the element
+
+newAttraction.innerHTML = 'Rent a Vespa'; // assigns element a text
+
+document.getElementById('italy-attractions').appendChild(newAttraction); // appends the element 
---
+++ Remove-an-Element/main.js
@@ -0,0 +1,5 @@
+let elementToRemove = document.getElementById('vespa'); 
+// Saves elementToRemove with ID 'vsepa'
+
+document.getElementById('italy-attractions').removeChild(elementToRemove); 
+// Removes elementToRemove from parent with ID 'italy-attractions'
